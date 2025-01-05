9d0b5a4b65e52a553440992b81611072d571510c
Akhil Joseph <akhiljosephaj85@gmail.com>
2025-01-06 01:53:02 +0530

    first commit

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,5 +1,4 @@
-const nodemailer = require('nodemailer');
-
+import nodemailer from 'nodemailer'
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { name, place, email, phone, message } = req.body;
