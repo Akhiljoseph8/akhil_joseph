e1c3a335c65a211430f788fa3a2e044b0fc42e79
Akhil Joseph <akhiljosephaj85@gmail.com>
2025-01-06 10:20:14 +0530

    first commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -110,7 +110,7 @@ const Home = () => {
           </div>
           <div class="col-lg-4 col-md-6">
             <div class="card text-center p-3 mt-2">
-              <img src="src/assets/images/web_deployment.jpg" alt=""></img>
+              <img src="../src/assets/images/web_deployment.jpg" alt=""></img>
               <div class="card-body">
                 <h4 class="card-title pt-5">Web Deployment</h4>
                 <a href="/services" class="btn more p-0">Explore More <i class="fas fa-long-arrow-alt-right ms-1"></i></a>
@@ -119,7 +119,7 @@ const Home = () => {
           </div>
           <div class="col-lg-4 col-md-6">
             <div class="card text-center p-3 mt-2">
-              <img src="src/assets/images/content_writing.jpg" alt=""></img>
+              <img src="../../../src/assets/images/content_writing.jpg" alt=""></img>
               <div class="card-body">
                 <h4 class="card-title pt-5">Content Writing</h4>
                 <a href="/services" class="btn more p-0">Explore More <i class="fas fa-long-arrow-alt-right ms-1"></i></a>
@@ -128,7 +128,7 @@ const Home = () => {
           </div>
           <div class="col-lg-4 col-md-6">
             <div class="card text-center p-3 mt-2">
-              <img src="src/assets/images/seo.jpg" alt=""></img>
+              <img src="../../src/assets/images/seo.jpg" alt=""></img>
               <div class="card-body">
                 <h4 class="card-title pt-5">SEO Implementation</h4>
                 <a href="/services" class="btn more p-0">Explore More <i class="fas fa-long-arrow-alt-right ms-1"></i></a>
